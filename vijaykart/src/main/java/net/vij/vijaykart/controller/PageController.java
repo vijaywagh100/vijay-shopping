0fb1a37999581c060b967ca7a8225442ae012d8e
vijay <vijaywagh100@gmail.com>
2018-01-01 20:29:32 +0530

    succusfully loded static resources

---
+++ vijaykart/src/main/java/net/vij/vijaykart/controller/PageController.java
@@ -0,0 +1,37 @@
+package net.vij.vijaykart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring Web MVC");
+		
+		return mv;
+	}
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam( value="greeting",required=false) String greeting,@RequestParam("year") String year){
+		if(greeting==null){
+			
+			greeting="welcome to 2018";
+		}
+		
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting",greeting+""+year);
+		
+		return mv;
+
+	}*/
+	
+	
+	
+	
+}
